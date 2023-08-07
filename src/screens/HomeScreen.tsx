77288162e1454bb121506b2f07a088b0d8b1df67
juam <juanmanso@gmail.com>
2023-08-07 15:57:34 +0200

    feat: init wallet on home screen with button press

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,18 +4,30 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
   useColorScheme,
 } from 'react-native';
 
+import { useWalletContext } from '../context/walletContext';
+
 export const HomeScreen = (): JSX.Element => {
   const isDarkMode = useColorScheme() === 'dark';
+  const { initWallet, wallet } = useWalletContext();
+
+  const isWalletInit = !!wallet;
+  const handleOnPress = isWalletInit ? undefined : initWallet;
 
   return (
     <SafeAreaView style={styles.mainContainer}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <View style={styles.container}>
         <Text>Hello World!</Text>
+        <TouchableOpacity onPress={handleOnPress} style={styles.button}>
+          <Text style={styles.whiteText}>
+            {isWalletInit ? 'Wallet Ready' : 'Wallet initiating...'}
+          </Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -29,5 +41,14 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     gap: 16,
+  },
+  button: {
+    borderRadius: 8,
+    backgroundColor: '#0066FF',
+    padding: 16,
+    alignSelf: 'flex-start',
+  },
+  whiteText: {
+    color: 'white',
   },
 });
