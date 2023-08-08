fea9df7dde64dcf437ed6a517072c60b13507570
juam <juanmanso@gmail.com>
2023-08-08 14:00:10 +0200

    feat: add gesture handler wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { WalletProvider } from './context/walletContext';
 import { Navigation } from './navigation';
@@ -8,7 +10,15 @@ import '../polyfills';
 export const App = (): JSX.Element => {
   return (
     <WalletProvider>
-      <Navigation />
+      <GestureHandlerRootView style={styles.flex}>
+        <Navigation />
+      </GestureHandlerRootView>
     </WalletProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
+});
